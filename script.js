672b9f1573dddba758341b305ffeb1864dba6dcd
Mari Umbelino <marivumbelino@gmail.com>
2026-03-12 16:21:07 -0300

    Add function to create and manage tasks

---
+++ script.js
@@ -0,0 +1,20 @@
+function addTask() {
+
+    const input = document.getElementById("taskInput");
+    const taskText = input.value;
+
+    if (taskText === "") {
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    li.onclick = function () {
+        li.style.textDecoration = "line-through";
+    };
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
